77b9ef3e8fa1552213f95de493964f7ae401bd76
felipesilva <tqi_fsilva@uolinc.com>
2022-01-24 18:26:07 -0300

    Add parent-pom to project

--- store/src/main/java/br/com/felipesilva/store/controller/StoreController.java
+++ store/src/main/java/br/com/felipesilva/store/controller/StoreController.java
@@ -1,14 +1,19 @@
 package br.com.felipesilva.store.controller;
 
 import br.com.felipesilva.store.dto.Purchase;
+import br.com.felipesilva.store.service.PurchaseService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/store")
 public class StoreController {
+
+    private final PurchaseService purchaseService;
 
     @PostMapping
     public void purchase(@RequestBody Purchase purchaseDTO) {
---
+++ store/src/main/java/br/com/felipesilva/store/service/PurchaseService.java
@@ -0,0 +1,7 @@
+package br.com.felipesilva.store.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PurchaseService {
+}
